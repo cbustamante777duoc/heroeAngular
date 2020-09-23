a9abcfd2c529dcc67469dd21dece3a801f7039ad
cristian <cra.bustamante@alumnos.duoc.cl>
2020-09-22 23:44:02 -0400

    aplicacion de heroe primer repositorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//rutas
+import {APP_ROUTING} from './app.routes';
+
+//servicios
+
+import {HeroesService} from './servicios/heroes.service';
+
+//componetes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componets/shared/navbar/navbar.component';
+import { HomeComponent } from './componets/home/home.component';
+import { AboutComponent } from './componets/about/about.component';
+import { HeroesComponent } from './componets/heroes/heroes.component';
+import { HeroeComponent } from './componets/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './componets/home/home.component'
+import {AboutComponent} from './componets/about/about.component'
+import {HeroesComponent} from './componets/heroes/heroes.component'
+import {HeroeComponent} from './componets/heroe/heroe.component'
+
+ const APP_ROUTES: Routes = [
+    {path:'home', component: HomeComponent},
+    {path:'about', component: AboutComponent},
+    {path:'heroes', component: HeroesComponent},
+    {path:'heroe/:id', component: HeroeComponent},
+    {path:'**', pathMatch:'full', redirectTo:'home'}
+    
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);

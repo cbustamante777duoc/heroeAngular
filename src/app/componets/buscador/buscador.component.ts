0d2bd4a24194d75d2375335d1bf8f8dc2c50d25d
cristian <cra.bustamante@alumnos.duoc.cl>
2020-09-24 01:01:31 -0400

    agregando funcion a buscador

--- src/app/componets/buscador/buscador.component.ts
+++ src/app/componets/buscador/buscador.component.ts
@@ -8,16 +8,30 @@ import { HeroesService } from "../../servicios/heroes.service";
   styles: []
 })
 export class BuscadorComponent implements OnInit {
+  heroes:any[] = [];
+  termino:string;
 
-  constructor(private activatedRoute:ActivatedRoute) {
+
+  constructor(private activatedRoute:ActivatedRoute,
+              private _heroesService:HeroesService ) {
 
    }
 
   ngOnInit() {
 
     this.activatedRoute.params.subscribe(params =>{
-      console.log(params['termino']);
+      this.termino = params['termino'];
+      this.heroes =this._heroesService.buscarHeroes(params['termino']);
+      console.log(this.heroes);
     })
   }
+
+  verHeroe(idx: number) {
+    console.log(idx)
+    // this.router.navigate(['/heroe', idx]);
+  }
+   
+
+  
 
 }
